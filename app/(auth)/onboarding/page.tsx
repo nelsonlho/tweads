1d7abdf72235186eb86bb9ec381ba03cc7565b2f
nelsonlho <nelsonlho@gmail.com>
2023-08-04 16:37:12 -0700

    starting clerk

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type Props = {};
+
+const Onboarding = (props: Props) => {
+  return <div>Onboarding</div>;
+};
+
+export default Onboarding;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl">Heading 1</h1>
+      <div className="text-4xl">Heading 1</div>
+    </main>
+  );
+}
