834a07470396b4cf7f7e0e03cb17a6d9297cdc28
nelsonlho <nelsonlho@gmail.com>
2023-08-08 01:14:52 -0700

    added card for threads

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -49,3 +49,67 @@ export async function createThread({
     throw new Error(`Failed to create thread: ${error.message}`);
   }
 }
+
+export async function fetchThreads(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip
+  const skipAmount = (pageNumber - 1) * pageSize;
+  // Fetch posts with no parents (only threads, not comments to threads)
+  const threadsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: 'desc' })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({ path: 'author', model: User })
+    .populate({
+      path: 'children',
+      populate: {
+        path: 'author',
+        model: User,
+        select: '_id name parentId image',
+      },
+    });
+
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  });
+
+  const threads = await threadsQuery.exec();
+  const isNext = totalPostsCount > skipAmount + threads.length;
+  return { threads, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+  try {
+    connectToDB();
+    const thread = await Thread.findById(id)
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id id name image',
+      })
+      .populate({
+        path: 'children',
+        populate: [
+          {
+            path: 'author',
+            model: User,
+            select: '_id id name image',
+          },
+          {
+            path: 'children',
+            model: Thread,
+            populate: {
+              path: 'author',
+              model: User,
+              select: '_id id name image',
+            },
+          },
+        ],
+      })
+      .exec();
+    return thread;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch thread: ${error.message}`);
+  }
+}
